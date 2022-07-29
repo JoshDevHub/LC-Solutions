8a59ef4e03177914ee61490f2d8b3a8068d1af46
jmsmith1018 <jmsmith1018@gmail.com>
2022-07-28 23:51:56 -0600

    add solution for 01 Matrix in ruby

---
+++ ruby_solutions/medium/01_matrix.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "set"
+
+def update_matrix(mat)
+  queue, visited = init_structures(row_size, col_size, mat)
+  bfs(queue, visited, mat)
+  mat
+end
+
+def bfs(queue, visited, matrix)
+  directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  until queue.empty?
+    queue.size.times do
+      i, j = queue.shift
+      directions.each do |(r, c)|
+        new_i = i + r
+        new_j = j + c
+        next if visited.include?([new_i, new_j]) || invalid_indices?(new_i, new_j, matrix)
+
+        queue << [new_i, new_j]
+        visited.add([new_i, new_j])
+        matrix[new_i][new_j] = matrix[i][j] + 1
+      end
+    end
+  end
+end
+
+def invalid_indices?(test_i, test_j, matrix)
+  row_size = matrix.size
+  col_size = matrix[0].size
+  test_i.negative? || test_j.negative? || test_i >= row_size || test_j >= col_size
+end
+
+def init_structures(matrix)
+  set = Set.new
+  queue = []
+  matrix.size.times do |i|
+    matrix[0].size.times do |j|
+      if matrix[i][j].zero?
+        queue << [i, j]
+        set.add([i, j])
+      end
+    end
+  end
+  [queue, set]
+end
--- ruby_solutions/medium/NOTES.md
+++ ruby_solutions/medium/NOTES.md
@@ -7,9 +7,20 @@ Given an integer array nums, return all distinct triplets that sum to 0.
 **Time Complexity**  
 O(n^2)
 
-**Solution Pseudo**  
+**Solution**  
 - Sort input array
 - Loop through input array
 - Maintain a left pointer just ahead of current element and right pointer at the end of array
 - Binary Search style manipulation of two pointers to find 0 sum combination
 - Left pointer manipulation to avoid duplicates
+
+## 01 Matrix
+
+Given a matrix filled with 1s and 0s, return the distance of the nearest 0 for each cell
+
+**Time Complexity**
+O(n^2)
+
+**Solution**
+- Build queue and visit set with all zero positions
+- BFS search
