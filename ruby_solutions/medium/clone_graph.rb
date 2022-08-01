f995359c311d4cbcb662d14835e8203d0d9a046d
jmsmith1018 <jmsmith1018@gmail.com>
2022-07-31 22:52:31 -0600

    add variant to ruby solution for Clone Graph

--- ruby_solutions/medium/clone_graph.rb
+++ ruby_solutions/medium/clone_graph.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Given class
 class Node
   attr_accessor :val, :neighbors
 
@@ -10,6 +11,26 @@ class Node
   end
 end
 
-def clone_graph(node)
+# Non-Algo Solution
+def clone_graph_marshal(node)
   Marshal.load(Marshal.dump(node))
+end
+
+# Algo Solution
+def clone_graph(node)
+  return node if node.nil?
+
+  clone(node)
+end
+
+def clone(node, found = {})
+  return found[node.val] if found.key?(node.val)
+
+  new_node = Node.new(node.val)
+  found[node.val] = new_node
+  neighbor_list = node.neighbors.map do |neighbor|
+    clone(neighbor, found)
+  end
+  new_node.neighbors = neighbor_list
+  new_node
 end
